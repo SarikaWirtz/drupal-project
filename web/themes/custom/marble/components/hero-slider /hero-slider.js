6a0e1b61b828ff1916ce62840df0efeb7bfc5325
Sarika Wirtz <sarikasingh223@gmail.com>
2025-11-30 22:40:05 +0100

    #3:install max length module and use tailwind for css

--- web/themes/custom/marble/components/hero-slider /hero-slider.js
+++ web/themes/custom/marble/components/hero-slider /hero-slider.js
@@ -1,20 +1,23 @@
-console.log("i am from component");
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.hero-slider-component').forEach(slider => {
     const options = slider.dataset.heroSliderOptions
       ? JSON.parse(slider.dataset.heroSliderOptions)
       : {};
-
-    new Splide(slider, Object.assign({
-      type: 'loop',
-      autoplay: true,
-      interval: 5000,
-      speed: 600,
-      rewind: true,
+    // Initialize Splide
+    const splide = new Splide(slider, Object.assign({
+      type        : 'loop',
+      autoplay    : true,
+      pauseOnFocus: true,
+      interval    : 5000,
+      speed       : 600,
+      rewind      : true,
       pauseOnHover: true,
-      arrows: true,
-      pagination: true,
-    }, options)).mount();
+      arrows      : true,
+      pagination  : true,
+      a11y        : true,  // Enable ARIA support
+      lazyLoad    : 'nearby',
+    }, options))
+    splide.mount();
   });
 });
-
